0f84896a4a570ebb755bb83554cdd18ef7ee72ea
baiching <bachmarma@gmail.com>
2025-11-12 12:28:26 +0100

    removed unnecessary comments

--- memory/memorytracker.h
+++ memory/memorytracker.h
@@ -18,11 +18,6 @@
 #define malloc(size) f_malloc_tracker(size, #size, __FILE__, __LINE__) // Replaces malloc
 #define free(ptr) f_track_free(ptr, #ptr, __FILE__, __LINE__)
 
-// #ifdef INTERNAL
-// #define realloc(ptr, size) f_realloc_tracker(ptr, size, __FILE__, __LINE__) // Replaces realloc
-// #endif
-
-
 extern void *f_malloc_tracker(size_t size, const char *expr, const char *file, int line); // allocates with malloc, adds it to the list to track and returns the allotted address for user
 extern void f_track_free(void *ptr, const char *expr, const char *file, int line); // free's the allocated memory and removes that from tracking list
 extern void f_trackListAllocations(); // returns the list of unfreed malloc's filename and line numbers
